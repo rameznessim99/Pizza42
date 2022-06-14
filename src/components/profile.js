895f3a25727311676cc846c0d9285863de741c53
Ramez Nessim <rameznessim99@gmail.com>
2022-06-14 14:15:08 -0400

    final updates- authentication

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,22 +1,36 @@
-import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
+import { Container, Row, Col } from "reactstrap";
 
-const Profile = () => {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+import Highlight from "../components/Highlight";
+import Loading from "../components/Loading";
+import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
+export const ProfileComponent = () => {
+  const { user } = useAuth0();
 
   return (
-    isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-    )
+    <Container className="mb-5">
+      <Row className="align-items-center profile-header mb-5 text-center text-md-left">
+        <Col md={2}>
+          <img
+            src={user.picture}
+            alt="Profile"
+            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
+          />
+        </Col>
+        <Col md>
+          <h2>{user.name}</h2>
+          <p className="lead text-muted">{user.email}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Highlight>{JSON.stringify(user, null, 2)}</Highlight>
+        <h2>{user.phone_number}</h2>
+      </Row>
+    </Container>
   );
 };
 
-export default Profile;
+export default withAuthenticationRequired(ProfileComponent, {
+  onRedirecting: () => <Loading />,
+});
